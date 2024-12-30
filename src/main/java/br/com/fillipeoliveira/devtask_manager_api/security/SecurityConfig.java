b7613ce0edd64743c70f99721ec5c9ea90469482
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-12-30 16:54:04 -0300

    configurando o spring security

---
+++ src/main/java/br/com/fillipeoliveira/devtask_manager_api/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package br.com.fillipeoliveira.devtask_manager_api.security;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class SecurityConfig {
+
+  private static final String[] PERMIT_ALL_LIST = {
+      "/swagger-ui/**",
+      "/v3/api-docs/**",
+      "/swagger-resource/**",
+      "/actuator/**"
+  };
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        // .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/users").permitAll()
+              .requestMatchers(PERMIT_ALL_LIST).permitAll();
+          // .requestMatchers(org.springframework.http.HttpMethod.OPTIONS,
+          // "/**").permitAll();
+          auth.anyRequest().authenticated();
+        });
+
+    return http.build();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  // @Bean
+  // CorsFilter corsFilter() {
+  //   UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  //   CorsConfiguration config = new CorsConfiguration();
+  //   config.setAllowCredentials(true);
+  //   config.addAllowedOriginPattern("*");
+  //   config.addAllowedHeader("*");
+  //   config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+  //   source.registerCorsConfiguration("/**", config);
+  //   return new CorsFilter(source);
+  // }
+
+  // @Bean
+  // UrlBasedCorsConfigurationSource corsConfigurationSource() {
+  //   UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  //   CorsConfiguration config = new CorsConfiguration();
+  //   config.setAllowCredentials(true);
+  //   config.addAllowedOriginPattern("*");
+  //   config.addAllowedHeader("*");
+  //   config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+  //   source.registerCorsConfiguration("/**", config);
+  //   return source;
+  // }
+}
