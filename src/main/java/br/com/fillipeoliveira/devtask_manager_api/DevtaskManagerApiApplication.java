2237972e9fdd362286e4a522108c83dd94676b66
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-12-30 00:06:34 -0300

    commit innital

---
+++ src/main/java/br/com/fillipeoliveira/devtask_manager_api/DevtaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fillipeoliveira.devtask_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtaskManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtaskManagerApiApplication.class, args);
+	}
+
+}
